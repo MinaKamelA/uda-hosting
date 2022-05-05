264f0b99849e3990cc968136d7c03eef50011f26
MinaKamelA <developmentmina@gmail.com>
2022-05-05 03:27:33 +0200

    change envrionment apihost

--- udagram-frontend/src/environments/environment.ts
+++ udagram-frontend/src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'Udagramapi-env.eba-yh72ejkg.us-east-1.elasticbeanstalk.com/api/v0'
+  apiHost: 'http://Udagramapi-env.eba-yh72ejkg.us-east-1.elasticbeanstalk.com/api/v0'
 };
 
 /*
